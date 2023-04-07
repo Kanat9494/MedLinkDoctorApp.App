b381a2eb81c547b41d3a6cfab31df430cc6880b0
Kanat9494 <kkanat94@gmail.com>
2023-04-07 18:19:55 +0600

    Add project files.

---
+++ MedLinkDoctorApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MedLinkDoctorApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
